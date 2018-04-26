cdc40ef04fe4c12266aeb6c1d9071ac1a9770d62
lipanlin <v-PanLin.Li@bl.com>
2018-04-26 11:21:18 +0800

    test

---
+++ springCloud-demo/src/main/java/com/bailian/springCloud/demo/App.java
@@ -0,0 +1,14 @@
+package com.bailian.springCloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+//@EnableEurekaServer
+@EnableConfigServer
+@SpringBootApplication
+public class App {
+    public static void main( String[] args ){
+        SpringApplication.run(App.class, args);
+    }
+}
