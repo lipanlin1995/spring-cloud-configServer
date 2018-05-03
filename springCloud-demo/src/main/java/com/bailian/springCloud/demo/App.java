b85e313d72a0069c101e45877874796faf65bdaf
lipanlin <v-PanLin.Li@bl.com>
2018-05-03 15:45:54 +0800

    test

--- springCloud-demo/src/main/java/com/bailian/springCloud/demo/App.java
+++ springCloud-demo/src/main/java/com/bailian/springCloud/demo/App.java
@@ -3,12 +3,16 @@ package com.bailian.springCloud.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RestController;
 
-//@EnableEurekaServer
-@EnableConfigServer
 @SpringBootApplication
+@EnableEurekaServer
+@EnableConfigServer
+@RestController
 public class App {
     public static void main( String[] args ){
         SpringApplication.run(App.class, args);
     }
+    
 }
