4bd95835a1551e420a6ecf09dfb61fb45cb4e6d6
lipanlin <v-PanLin.Li@bl.com>
2018-05-03 16:28:38 +0800

    test

--- springCloud-demo/src/main/java/com/bailian/springCloud/demo/App.java
+++ springCloud-demo/src/main/java/com/bailian/springCloud/demo/App.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class App {
     public static void main( String[] args ){
+        //dd
         SpringApplication.run(App.class, args);
     }
     
